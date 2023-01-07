882280b70fd482d6108aae0ed69d2f6a8cb5ecd7
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-01-07 17:03:23 +0530

    signIn and SignUp added

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import AuthForm from "./Components/AuthForm";
+
+function App() {
+  return <AuthForm />;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import "../node_modules/react-bootstrap/dist/react-bootstrap"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import { Provider } from 'react-redux';
+import Store from "./store/Store"
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={Store}>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+    </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
