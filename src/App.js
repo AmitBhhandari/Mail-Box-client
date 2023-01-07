6dc1cebe80d758d0294135d801d64c0ab6f9c6cd
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-01-07 21:04:44 +0530

    compose and send mail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import AuthForm from "./Components/AuthForm";
+import TextEditing from "./Components/TextEditing/TextEditing";
 
 function App() {
-  return <AuthForm />;
+  return <TextEditing/>
 }
 
 export default App;
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "./Auth";
 import Uislice from "./Uivisible";
+import MailSlice from "./MailSlice";
 
 const Store = configureStore({
   reducer: {
     auth: AuthSlice.reducer,
     uiauth: Uislice.reducer,
+    mail: MailSlice.reducer,
   },
 });
 export default Store;
